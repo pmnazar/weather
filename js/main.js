c6e6d2b8817ef886e22aa373a8217259ee8d2a51
npylypyak <Pylypyaknazar@gmail.com>
2016-11-05 16:11:43 +0200

    Add fig loading while send ajax

--- js/main.js
+++ js/main.js
@@ -10,35 +10,39 @@
 		var city = document.getElementById( 'city' ),
 			search = document.getElementById( 'search' ),
 			xhr = new XMLHttpRequest(),
+			loading = document.getElementById( 'loading' ),
 			apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&appid=' + apiId + '&units=metric';
+		
 		city.value = '';
+		loading.style.display = 'block';
 		xhr.open( 'GET', apiUrl, true );
 		
 		xhr.send();
 		
 		xhr.onreadystatechange = function () {
-			if ( this.readyState !== 4 ) return false;
-			
-			if ( this.status === 200 ) {
-				var res = JSON.parse( this.responseText ),
-					li = document.createElement( 'li' ),
-					span = document.createElement( 'span' );
-				console.log( res );
-				span.innerHTML = res.name; // set city name
-				
-				li.className = 'weather__list-result-item';
-				li.appendChild( span );
-				
-				li.innerHTML = '— ' + li.innerHTML + ', ' + res.sys.country + ': ' + res.main.temp + '°C, ' + res.weather[ 0 ].description;
-				// check length result list, append elem first
-				if ( listResult.childNodes.length >= 1 ) {
-					listResult.insertBefore( li, listResult.firstElementChild )
+			if ( this.readyState === 4 ) {
+				loading.style.display = 'none';
+				if ( this.status === 200 ) {
+					var res = JSON.parse( this.responseText ),
+						li = document.createElement( 'li' ),
+						span = document.createElement( 'span' );
+					
+					span.innerHTML = res.name; // set city name
+					
+					li.className = 'weather__list-result-item';
+					li.appendChild( span );
+					
+					li.innerHTML = '— ' + li.innerHTML + ', ' + res.sys.country + ': ' + res.main.temp + '°C, ' + res.weather[ 0 ].description;
+					// check length result list, append elem first
+					if ( listResult.childNodes.length >= 1 ) {
+						listResult.insertBefore( li, listResult.firstElementChild )
+					} else {
+						listResult.appendChild( li );
+					}
 				} else {
-					listResult.appendChild( li );
+					console.error( 'Error: ' + (this.status ? this.statusText : 'request failed') );
+					return false;
 				}
-			} else {
-				console.error( 'Error: ' + (this.status ? this.statusText : 'request failed') );
-				return false;
 			}
 		}
 	} );
